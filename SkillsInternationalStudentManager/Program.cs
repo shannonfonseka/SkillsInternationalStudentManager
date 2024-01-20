871f2fd58248cc6efbd4b39a7d8745d49639f75f
Shannon Fonseka <fonsekashannonshiwantha@gmail.com>
2024-01-20 22:30:52 +0530

    Added mutex for single instance

--- SkillsInternationalStudentManager/Program.cs
+++ SkillsInternationalStudentManager/Program.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+
 namespace SkillsInternationalStudentManager
 {
     internal static class Program
@@ -5,13 +7,36 @@ namespace SkillsInternationalStudentManager
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        // Mutex can be made static so that GC doesn't recycle
+        // same effect with GC.KeepAlive(mutex) at the end of main
+        static Mutex mutex = new Mutex(false, "some-unique-id");
+
         [STAThread]
         static void Main()
         {
+            // if you like to wait a few seconds in case that the instance is just 
+            // shutting down
+            if (!mutex.WaitOne(TimeSpan.FromSeconds(2), false))
+            {
+                MessageBox.Show("Application already started!", "", MessageBoxButtons.OK);
+                return;
+            }
+
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(true);
+                ApplicationConfiguration.Initialize();
+                Application.Run(new frmSplashScreen());
+            }
+            finally { mutex.ReleaseMutex(); } // I find this more explicit
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new frmSplashScreen());
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new frmSplashScreen());
         }
     }
 }
